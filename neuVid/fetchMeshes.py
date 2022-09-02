bbae5c8e2b8596f9bc5d612314ee82c8dd93b0a2
Philip M. Hubbard <hubbardp@janelia.hhmi.org>
2022-09-02 14:08:12 -0400

    Add support for OpenOrganelle sources.

--- neuVid/fetchMeshes.py
+++ neuVid/fetchMeshes.py
@@ -7,8 +7,12 @@
 # $ python fetchMeshes.py -ij video.json
 
 import argparse
+import DracoPy
 import json
+import numpy as np
 import os
+import requests
+import struct
 import sys
 import tempfile
 import trimesh
@@ -16,9 +20,26 @@ from meshparty import trimesh_io
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 from utilsJson import decode_id, parseNeuronsIds, removeComments
-from utilsNg import dir_name_from_ng_source, is_ng_source
+from utilsNg import dir_name_from_ng_source, is_ng_source, source_to_url
 
-def fetch_ng_source(source, decim_fraction):
+def get_mesh_info(source):
+    url = source_to_url(source)
+    if url:
+        try:
+            response = requests.get(url + "/info")
+            response.raise_for_status()
+            info = response.json()
+            return info
+        except requests.exceptions.RequestException as e:
+            print(f"Error: get({url}) failed: {str(e)}")
+    return None
+
+def is_cloudvolume_accessible(mesh_info):
+    return "scales" in mesh_info
+
+# Supports Neuroglancer precomputed with sharding, but fails when the "scales" metadata is on another source.
+
+def fetch_with_cloudvolume(source, ids, decim_fraction, input_json_dir):
     dir = dir_name_from_ng_source(source)
 
     try:
@@ -29,48 +50,162 @@ def fetch_ng_source(source, decim_fraction):
         mesh_meta = trimesh_io.MeshMeta(cv_path=source, disk_cache_path=tmp_dir, map_gs_to_https=True)
 
         j = 0
-        for neuron_id in neuron_ids[i]:
-            neuron_id = decode_id(neuron_id)
-            neuron_id = int(neuron_id)
-            percent = j / len(neuron_ids[i]) * 100
+        for id in ids:
+            id = decode_id(id)
+            id = int(id)
+            percent = j / len(ids) * 100
 
-            print("[{:.1f}%] Fetching ID {} ...".format(percent, neuron_id))
-            mesh = mesh_meta.mesh(seg_id=neuron_id)
+            print(f"[{percent:.1f}%] Fetching ID {id} ...")
+            mesh = mesh_meta.mesh(seg_id=id)
             print("Done")
 
             face_count = mesh.faces.shape[0]
-            print("[{:.1f}%] Smoothing ID {} ...".format(percent, neuron_id))
+            print(f"[{percent:.1f}%] Smoothing ID {id} ...")
             mesh_smooth = trimesh.smoothing.filter_taubin(mesh)
             print("Done")
 
-            print("[{:.1f}%] Decimating ID {} ...".format(percent, neuron_id))
+            print(f"[{percent:.1f}%] Decimating ID {id} ...")
             mesh_smooth_decim = mesh_smooth.simplify_quadratic_decimation(face_count * decim_fraction)
             print("Done")
 
-            output = os.path.join(download_dir, str(neuron_id) + ".obj")
-            print("[{:.1f}%] Exporting {} ...".format(percent, output))
+            output = os.path.join(download_dir, str(id) + ".obj")
+            print(f"[{percent:.1f}%] Exporting {output} ...")
             mesh_smooth_decim.export(output)
             print("Done")
 
             j += 1
 
     except OSError as e:
-        print("Error: fetching from source '{}' failed: {}".format(source, str(e)))
+        print(f"Error: fetching from source '{source}' failed: {str(e)}")
+
+# Supports the approach used by OpenOrganelle, with an N5 volume source having the "scales" metadata
+# and another source having the meshes in Neuroglancer precomputed format.
+# Does not support sharding.
+# Based on code from David Ackerman.
+
+def unpack_and_remove(datatype, num_elements, file_content):
+    datatype = datatype * num_elements
+    output = struct.unpack(datatype, file_content[0:4 * num_elements])
+    file_content = file_content[4 * num_elements:] 
+    return np.array(output), file_content
+
+def fetch_directly(source, mesh_info, ids, lod, decim_fraction, input_json_dir):
+    url_base = source_to_url(source)
+    if not url_base:
+        return
+
+    # Get quantization and transform, for computing voxel size.
+    if not "vertex_quantization_bits" in mesh_info:
+        return
+    vertex_quantization_bits = mesh_info["vertex_quantization_bits"]
+    if not "transform" in mesh_info:
+        return
+    meshes_transform = mesh_info["transform"]
+    meshes_transform += [0, 0, 0, 1]
+    meshes_transform = np.reshape(meshes_transform, (4, 4))
+
+    dir = dir_name_from_ng_source(source)   
+    download_dir = os.path.join(input_json_dir, dir)
+    if not os.path.exists(download_dir):
+        os.mkdir(download_dir)
+
+    failed = []
+
+    j = 0
+    for id in ids:
+        id = decode_id(id)
+        percent = j / len(ids) * 100
+        print(f"[{percent:.1f}%] Fetching ID {id} ...")
+
+        # Get index file info.
+        # Doing so may require several tries if there is "500 Server Error: Internal Server Error".
+        success = False
+        tries = 0
+        while tries < 5:
+            try:
+                url = f"{url_base}/{id}.index"
+                response = requests.get(url)
+                response.raise_for_status()
+                index_file_content = response.content
+                success = True
+                break
+            except requests.exceptions.RequestException as e:
+                print(f"Error: get({url}) failed: {str(e)}")
+                tries += 1
+        if not success:
+            failed.append(url)
+            continue
+
+        chunk_shape, index_file_content = unpack_and_remove("f", 3, index_file_content)
+        grid_origin, index_file_content = unpack_and_remove("f", 3, index_file_content)
+        num_lods, index_file_content = unpack_and_remove("I", 1, index_file_content)
+        lod_scales, index_file_content = unpack_and_remove("f", num_lods[0], index_file_content)
+        vertex_offsets, index_file_content = unpack_and_remove("f", num_lods[0] * 3, index_file_content)
+        num_fragments_per_lod, index_file_content = unpack_and_remove("I", num_lods[0], index_file_content)
+
+        previous_lod_byte_offset = 0
+        for current_lod in range(lod + 1):
+            fragment_positions, index_file_content = unpack_and_remove("I", num_fragments_per_lod[current_lod] * 3, index_file_content)
+            fragment_positions = fragment_positions.reshape((3, -1)).T
+            fragment_offsets, index_file_content = unpack_and_remove("I", num_fragments_per_lod[current_lod], index_file_content)
+
+            lod_byte_offset = np.cumsum(np.array(fragment_offsets)) + previous_lod_byte_offset
+            lod_byte_offset = np.insert(lod_byte_offset, 0, previous_lod_byte_offset)
+            # End of previous LOD.
+            previous_lod_byte_offset = lod_byte_offset[-1]
+
+        mesh_fragments = []
+        for idx,fragment_offset in enumerate(fragment_offsets):
+            if (lod_byte_offset[idx] != lod_byte_offset[idx + 1]): 
+                # Nonempty chunk.
+                chunk_name = f"{id}_{fragment_positions[idx][0]}_{fragment_positions[idx][1]}_{fragment_positions[idx][2]}"
+                response = requests.get(f'{url_base}/{id}', headers={"range": f"bytes={lod_byte_offset[idx]}-{lod_byte_offset[idx+1]}"})
+                drc_mesh = DracoPy.decode(response.content)
+                trimesh_mesh = trimesh.Trimesh(vertices=drc_mesh.points, faces=drc_mesh.faces)
+                n = chunk_shape * (2**lod) * (fragment_positions[idx] + trimesh_mesh.vertices / (2**vertex_quantization_bits - 1))
+                trimesh_mesh.vertices = grid_origin + vertex_offsets[lod] + n
+                mesh_fragments.append(trimesh_mesh)
+
+        mesh = trimesh.util.concatenate(mesh_fragments)
+        mesh.merge_vertices()
+        mesh.apply_transform(meshes_transform)
+        print("Done")
+
+        face_count = mesh.faces.shape[0]
+        print(f"[{percent:.1f}%] Smoothing ID {id} ...")
+        mesh_smooth = trimesh.smoothing.filter_taubin(mesh)
+        print("Done")
+
+        print(f"[{percent:.1f}%] Decimating ID {id} ...")
+        mesh_smooth_decim = mesh_smooth.simplify_quadratic_decimation(face_count * decim_fraction)
+        print("Done")
+
+        output = os.path.join(download_dir, str(id) + ".obj")
+        mesh.export(output)
+
+        j += 1
+
+    if len(failed) > 0:
+        print(f"Failed: {failed}")
+
+#
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--inputJson", "-ij", dest="input_json_file", help="path to the JSON file describing the input")
-    parser.set_defaults(decim_fraction=0.25)
+    parser.set_defaults(decim_fraction=0.5)
     parser.add_argument("--decim", "-d", type=float, dest="decim_fraction", help="mesh decimation fraction")
+    parser.set_defaults(lod=3)
+    parser.add_argument("--lod", "-l", type=int, dest="lod", help="mesh LOD (level of detail), 1 is highest resolution")
     args = parser.parse_args()
 
-    print("Using input file: {}".format(args.input_json_file))
-    print("Using decimation fraction: {}".format(args.decim_fraction))
+    print(f"Using input file: {args.input_json_file}")
+    print(f"Using decimation fraction: {args.decim_fraction}")
 
     input_json_dir = os.path.dirname(os.path.realpath(args.input_json_file))
     json_data = json.loads(removeComments(args.input_json_file))
     if json_data == None:
-        print("Loading JSON file {} failed".format(args.input_json_file))
+        print(f"Loading JSON file {args.input_json_file} failed")
         quit()
 
     if "neurons" in json_data:
@@ -85,11 +220,16 @@ if __name__ == "__main__":
             neuron_ids, _, _, _ = parseNeuronsIds(json_neurons)
 
             for i in range(len(neuron_sources)):
-                if len(neuron_sources) == 1:
-                    print("Fetching {} neuron meshes".format(len(neuron_ids[i])))
-                else:
-                    print("Fetching {} neuron meshes for index {}".format(len(neuron_ids[i]), i))
-
                 source = neuron_sources[i]
+
+                if len(neuron_sources) == 1:
+                    print(f"Fetching {len(neuron_ids[i])} neuron meshes from source {source}")
+                else:
+                    print(f"Fetching {len(neuron_ids[i])} neuron meshes for source index {i}: {source}")
+
                 if is_ng_source(source):
-                    fetch_ng_source(source, args.decim_fraction)
+                    mesh_info = get_mesh_info(source)
+                    if is_cloudvolume_accessible(mesh_info):
+                        fetch_with_cloudvolume(source, neuron_ids[i], args.decim_fraction, input_json_dir)
+                    else:
+                        fetch_directly(source, mesh_info, neuron_ids[i], args.lod, args.decim_fraction, input_json_dir)
